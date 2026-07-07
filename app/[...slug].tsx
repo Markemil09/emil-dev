752e985b69584e48d2fc47730234bf979509917d
Mark Sarmiento <mark.emil.sarmiento@gmail.com>
2026-07-08 01:25:42 +0800

    feat: config destination

---
+++ app/[...slug].tsx
@@ -0,0 +1,18 @@
+import { View, ActivityIndicator, StyleSheet } from 'react-native';
+
+export default function ArticleShell() {
+  return (
+    <View style={styles.container}>
+      <ActivityIndicator />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#0a0a0a',
+  },
+});
